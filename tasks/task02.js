1eb3f3fd6edb960bf30c284eb7f760ce2d869a2a
ArtemChepeliuk <4epeliuk@gmail.com>
2018-04-04 15:39:13 +0300

    fix task02

--- tasks/task02.js
+++ tasks/task02.js
@@ -2,7 +2,7 @@ function pifagor () {
 	var a = prompt("Enter a:");
 	var b = prompt("Enter b:");
 	var c = a * a + b * b;
-	var с = Math.sqrt(c);
+    с = Math.sqrt(c);
 	console.log(с);
 }
 pifagor();
