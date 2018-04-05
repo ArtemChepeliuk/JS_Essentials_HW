daa99f9a9e53fafc09abae567a2d6bd1cec62f90
ArtemChepeliuk <4epeliuk@gmail.com>
2018-04-05 22:07:23 +0300

    done task05

--- tasks/task05.js
+++ tasks/task05.js
@@ -1,8 +1,9 @@
-function getValueHrivna(){
-  var hrivna = document.getElementById("hrivna").value; 
-    document.getElementById("resultHrivna").value = hrivna / 25.885;
-  }
-function getValueDollar(){
-  var dollar = document.getElementById("dollar").value; 
-    document.getElementById("resultDollar").value = dollar * 25.885;
-  }
+function getValueHrivna() {
+	var hrivna = document.getElementById("hrivna").value;
+	document.getElementById("resultHrivna").value = hrivna / 25.885;
+}
+
+function getValueDollar() {
+	var dollar = document.getElementById("dollar").value;
+	document.getElementById("resultDollar").value = dollar * 25.885;
+}
