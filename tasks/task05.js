412237648add20c364685a11143963a3ec66bda7
ArtemChepeliuk <4epeliuk@gmail.com>
2018-04-05 18:53:17 +0300

    done task05

--- tasks/task05.js
+++ tasks/task05.js
@@ -1,24 +1,8 @@
-
 function getValueHrivna(){
   var hrivna = document.getElementById("hrivna").value; 
-  	hrivna = hrivna * 25.885;
-  // document.getElementById("resultHrivna").innerHTML = hrivna;
-  console.log(hrivna);
+    document.getElementById("resultHrivna").value = hrivna / 25.885;
   }
-// getValueHrivna();
-var resultHrivna = document.getElementById('resultHrivna')	
- resultHrivna.value = getValueHrivna(); 
-
-
-// function getValueDollar(){
-//   var dollar = document.getElementById("dollar").value; 
-//   	dollar = dollar / 25.885;
-//   // document.getElementById("resultHrivna").innerHTML = hrivna;
-//   console.log(dollar);
-//   }
-// // getValueHrivna();
-
-// document.getElementById("resultDollar").value = getValueDollar();
-
-
-
+function getValueDollar(){
+  var dollar = document.getElementById("dollar").value; 
+    document.getElementById("resultDollar").value = dollar * 25.885;
+  }
