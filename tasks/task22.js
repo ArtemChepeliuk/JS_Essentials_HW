2571a2b895fbb2e772f07a13889939c1402d11bd
ArtemChepeliuk <4epeliuk@gmail.com>
2018-04-11 19:49:30 +0300

    add task26

--- tasks/task22.js
+++ tasks/task22.js
@@ -1,3 +1,9 @@
-function combinOfString(str) {
- 	
- } 
+function combinStr(str) {
+    for (var i = 0; i < str.length; i++) {
+        for (var j = i+1; j < str.length+1; j++){
+            console.log(str.slice(i, j));
+        }
+    }
+}
+
+combinStr("dog");
---
+++ tasks/task26.js
@@ -0,0 +1,7 @@
+function factorial(n) {
+	for (var j=n-1; j>0; --j){
+     n = n*j    
+}
+console.log(n);
+}
+factorial(5);
