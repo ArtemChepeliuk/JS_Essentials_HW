c8b969262f1af12ebb6457b14fe9c5eca97618ca
ArtemChepeliuk <4epeliuk@gmail.com>
2018-04-12 15:02:59 +0300

    add task30

--- tasks/task30.js
+++ tasks/task30.js
@@ -1,9 +1,8 @@
-function reverseStr(str) {
-	var arr = str.split(''); // створюємо масив і розділяємо його елементи
-	for (var i = 0; i < arr.length; i++) {
-	}
-	arr.reverse()// записуємо масив в зворотньому порядку
-	var str = arr.join(""); ;  
-	console.log(str);
+function log10(i) 
+{
+  if (i>=0) {
+    console.log(i);
+    log10(i-1);
+  }
 }
-reverseStr("hello world");
+log10(5);
